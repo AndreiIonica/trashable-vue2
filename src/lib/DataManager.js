f59ef54a3d142399ea915f5d4794e3c6b975f8c6
Silviu2004 <silvius2409@gmail.com>
2021-03-13 20:43:44 +0200

    Finish DataManager

--- src/lib/DataManager.js
+++ src/lib/DataManager.js
@@ -23,8 +23,56 @@ async function creareCos(trashcan, token) {
   });
   return res.data;
 }
+async function updatareCos(trashcan, token) {
+  let res = await axios.put('https://trashable-server.herokuapp.com/api/0.1/trashcan/', trashcan, {
+    headers: {
+      'auth-token': token,
+    },
+  });
+  return res.data;
+}
+async function toateCosurile() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/trashcan/');
+  return res.data;
+}
+
+async function tipuriCosuri() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/trashcanType/');
+  return res.data;
+}
+
+async function orase() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/city/');
+  return res.data;
+}
+async function updatareCos(trashcan, token) {
+  let res = await axios.put('https://trashable-server.herokuapp.com/api/0.1/trashcan/', trashcan, {
+    headers: {
+      'auth-token': token,
+    },
+  });
+  return res.data;
+}
+async function toateCosurile() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/trashcan/');
+  return res.data;
+}
+
+async function tipuriCosuri() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/trashcanType/');
+  return res.data;
+}
+
+async function orase() {
+  let res = await axios.get('https://trashable-server.herokuapp.com/api/0.1/city/');
+  return res.data;
+}
 export default {
   signup,
   login,
   creareCos,
+  updatareCos,
+  toateCosurile,
+  tipuriCosuri,
+  orase,
 };
